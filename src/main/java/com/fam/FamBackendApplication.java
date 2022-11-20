5f632e5918fdaecf41342425cd2d2b453747fc21
rajsundardev <rajsundarprog@gmail.com>
2022-11-20 21:50:53 +0800

    commit all files

---
+++ src/main/java/com/fam/FamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamBackendApplication.class, args);
+	}
+
+}
